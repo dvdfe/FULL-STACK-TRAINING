9537b5fa90339b47687dc17d833b3125413cc9ee
DVDFE <davidfe.pro@gmail.com>
2023-06-29 16:22:24 +0200

    ajout de la base de donnée

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose")
+mongoose.connect('mongodb+srv://dvdfe:<password>@cluster0.idcid.mongodb.net/?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+
+
 app.use(express.json())
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
