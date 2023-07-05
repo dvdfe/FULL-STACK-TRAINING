0dc977abee8bb130a6a09dc792115d4770e2023b
DVDFE <davidfe.pro@gmail.com>
2023-07-05 22:28:02 +0200

    modification de controllers pour gerer les requetes avec images

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv").config();
 const router = require("./routes/stuff");
-const userRoutes = require("./routes/user")
+const userRoutes = require("./routes/user");
+const path = require("path")
 
 mongoose
   .connect(process.env.DB_ADMIN, {
@@ -27,7 +28,8 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use("/api/stuff", router)
-app.use("/api/auth", userRoutes)
+app.use("/api/stuff", router);
+app.use("/api/auth", userRoutes);
+app.use("/images", express.static(path.join(__dirname, "images")));
 
 module.exports = app;
--- routes/stuff.js
+++ routes/stuff.js
@@ -2,12 +2,13 @@ const express = require("express");
 const router = express.Router();
 const stuffCtrl = require("../controllers/stuff")
 const auth = require("../middleware/auth")
+const multer = require("../middleware/multer-config")
 
-router.post("/", auth, stuffCtrl.createThing) 
+router.post("/", auth, multer, stuffCtrl.createThing) 
 
 router.delete("/:id", auth, stuffCtrl.deleteThing)
 
-router.put("/:id", auth, stuffCtrl.modifyThing)
+router.put("/:id", auth, multer, stuffCtrl.modifyThing)
 
 router.get("/:id", auth, stuffCtrl.getOneThing)
 
